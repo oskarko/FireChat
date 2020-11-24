467beca8e1391d3e46519e5fc19a2f44e05f36a8
Óscar R. Garrucho <oscar.garrucho@gmail.com>
2020-11-24 09:51:02 +0100

    Setup messages tableView

--- FireChat/Controller/ConversationsController.swift
+++ FireChat/Controller/ConversationsController.swift
@@ -7,16 +7,21 @@
 
 import UIKit
 
+private let reuseIdentifier = "ConversationCell"
+
 class ConversationsController: UIViewController {
 
     // MARK: - Properties
+
+    private let tableView = UITableView()
 
     // MARK: - Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        configureUI()
         configureNavigationBar()
+        configureUI()
+        configureTableView()
     }
 
     // MARK: - Selectors
@@ -33,6 +38,18 @@ class ConversationsController: UIViewController {
                                                            style: .plain,
                                                            target: self,
                                                            action: #selector(showProfile))
+    }
+
+    private func configureTableView() {
+        tableView.backgroundColor = .white
+        tableView.rowHeight = 80
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: reuseIdentifier)
+        tableView.tableFooterView = UIView()
+        tableView.delegate = self
+        tableView.dataSource = self
+
+        view.addSubview(tableView)
+        tableView.frame = view.frame
     }
 
     private func configureNavigationBar() {
@@ -54,3 +71,25 @@ class ConversationsController: UIViewController {
         navigationController?.navigationBar.overrideUserInterfaceStyle = .dark
     }
 }
+
+extension ConversationsController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //
+    }
+}
+
+extension ConversationsController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath)
+        cell.textLabel?.text = "Test Cell"
+
+        return cell
+    }
+
+
+}
+
