8441b40fd46643d0622504dc35aa75663791907b
Óscar R. Garrucho <oscar.garrucho@gmail.com>
2020-11-24 07:05:13 +0100

    Initial Commit

---
+++ FireChat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FireChat
+//
+//  Created by Oscar Rodriguez Garrucho on 24/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
