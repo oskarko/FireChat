9f17120c305907c6483b0a12317777cfb15b3f15
Óscar R. Garrucho <oscar.garrucho@gmail.com>
2020-11-24 10:36:37 +0100

    Login screen UI

--- FireChat/Controller/Authentication/LoginController.swift
+++ FireChat/Controller/Authentication/LoginController.swift
@@ -18,6 +18,30 @@ class LoginController: UIViewController {
         return iv
     }()
 
+    private let emailContainerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .cyan
+        view.setHeight(height: 50)
+        return view
+    }()
+
+    private let passwordContainerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .yellow
+        view.setHeight(height: 50)
+        return view
+    }()
+
+    private let loginButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Log In", for: .normal)
+        button.layer.cornerRadius = 5
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
+        button.backgroundColor = .systemRed
+        button.setHeight(height: 50)
+        return button
+    }()
+
     // MARK: - Lifecycle
 
     override func viewDidLoad() {
@@ -35,7 +59,23 @@ class LoginController: UIViewController {
         configureGradientLayer()
 
         view.addSubview(iconImage)
-        iconImage
+        iconImage.centerX(inView: view)
+        iconImage.anchor(top: view.safeAreaLayoutGuide.topAnchor, paddingTop: 32)
+        iconImage.setDimensions(height: 120, width: 120)
+
+        let stack = UIStackView(arrangedSubviews: [emailContainerView,
+                                                   passwordContainerView,
+                                                   loginButton])
+        stack.axis = .vertical
+        stack.spacing = 16
+
+        view.addSubview(stack)
+        stack.anchor(top: iconImage.bottomAnchor,
+                     left: view.leftAnchor,
+                     right: view.rightAnchor,
+                     paddingTop: 32,
+                     paddingLeft: 32,
+                     paddingRight: 32)
     }
 
     private func configureGradientLayer() {
